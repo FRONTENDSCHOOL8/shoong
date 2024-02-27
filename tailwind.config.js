c9468529042eac14e09a5cfa3d649b2b005c58b8
KIMCHOONGMAN <cm730712@gmail.com>
2024-02-27 13:29:58 +0900

    [Feat] SUIT font 추가, pr 템플릿 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,19 +25,26 @@ export default {
       lineHeight: {
         ...pxToRemFunc(0, 1000),
       }, // px을 rem으로 변환
-    },
-    colors: {
-      primary: 'rgba(102, 98, 201, 1)',
-      secondary: 'rgba(124, 120, 224, 1)',
-      tertiary: 'rgba(210, 202, 250, 1)',
-      negative: 'rgba(225, 25, 0, 1)',
-      contentPrimary: 'rgba(32, 33, 35, 1)',
-      contentSecondary: 'rgba(109, 112, 126, 1)',
-      contentTertiary: 'rgba(157, 161, 180, 1)',
-    },
-    boxShadow: {
-      navShadow: '0px -2px 4px 0px rgba(0,0,0,0.1)',
-      meetUp: '0px 2px 20px 0px rgba(0,0,0,0.15)',
+      colors: {
+        primary: 'rgba(102, 98, 201, 1)',
+        secondary: 'rgba(124, 120, 224, 1)',
+        tertiary: 'rgba(210, 202, 250, 1)',
+        negative: 'rgba(225, 25, 0, 1)',
+        contentPrimary: 'rgba(32, 33, 35, 1)',
+        contentSecondary: 'rgba(109, 112, 126, 1)',
+        contentTertiary: 'rgba(157, 161, 180, 1)',
+      },
+      boxShadow: {
+        navShadow: '0px -2px 4px 0px rgba(0,0,0,0.1)',
+        meetUp: '0px 2px 20px 0px rgba(0,0,0,0.15)',
+      },
+      screens: {
+        mobile: '360px',
+        desktop: '1080px',
+      },
+      fontFamily: {
+        suit: ['SUIT', 'noto-sans-kr', 'sans-serif'],
+      },
     },
   },
   plugins: [],
