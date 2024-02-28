5662d2f44e2854db230ecf2ff97bf3cd43a8640a
roben31380 <kinalee0430@gmail.com>
2024-02-28 14:34:31 +0900

    [Feat] 기본 파일 생성

---
+++ src/components/FloattingButton/FloatingButton.stories.jsx
@@ -0,0 +1,10 @@
+import FloattingButton from './FloattingButton';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: FloattingButton,
+  args:
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
