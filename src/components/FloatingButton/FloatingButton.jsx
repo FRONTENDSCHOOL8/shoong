3115897964ead5e0deaa79a6cda6640ae1ed1292
roben31380 <kinalee0430@gmail.com>
2024-02-28 16:48:45 +0900

    [Feat] 마크업 완성

--- src/components/FloatingButton/FloatingButton.jsx
+++ src/components/FloatingButton/FloatingButton.jsx
@@ -1,14 +1,35 @@
 export default function FloatingButton() {
   return (
-    <button>
+    <button type="button" className="absolute bottom-0 right-0">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="55"
-        height="55"
-        viewBox="0 0 55 55"
+        width={60}
+        height={60}
+        viewBox="0 0 60 60"
         fill="none"
       >
-        <circle cx="27.5" cy="27.5" r="27.5" fill="white" />
+        <circle
+          cx="29"
+          cy="29"
+          r="25"
+          strokeWidth="2"
+          fill="white"
+          stroke="url(#paint0_linear_7194_2228)"
+        />
+        <image width={35} x={10} y={13} href="/icons/heartLogo.png" />
+        <defs>
+          <linearGradient
+            id="paint0_linear_7194_2228"
+            x1="29.5"
+            y1={0}
+            x2="29.5"
+            y2={59}
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#FE5F5F" />
+            <stop offset={1} stopColor="#6662C9" />
+          </linearGradient>
+        </defs>
       </svg>
     </button>
   );
