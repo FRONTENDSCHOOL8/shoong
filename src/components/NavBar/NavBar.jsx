b9158bb7b2faab0ed5fd4087acba4ddf4767ada2
KIMCHOONGMAN <cm730712@gmail.com>
2024-02-28 13:49:30 +0900

    [Feat] NavBarItem 컴포넌트 추가

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,14 @@
 import { GoArrowSwitch, GoPerson, GoLocation, GoHome } from 'react-icons/go';
-import { NavLink } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import NavBarItem from './NavBarItem';
 
 export default function NavBar() {
   return (
-    <div className="w-360pxr">
-      <ul className="flex items-center justify-around px-5 pb-3 gap-64pxr pt-10pxr shadow-navShadow">
-        <li className="cursor-pointer text-contentTertiary hover:text-primary">
-          <Link to="home" className="flex flex-col items-center"></Link>
-          <GoHome className="h-8 w-22pxr" />홈
-        </li>
-        <li className="cursor-pointer text-contentTertiary hover:text-primary">
-          <Link to="exchange" className="flex flex-col items-center">
-            <GoArrowSwitch className="h-8 w-22pxr" />
-            교환
-          </Link>
-        </li>
-        <li className="cursor-pointer text-contentTertiary hover:text-primary">
-          <Link to="meetup" className="flex flex-col items-center">
-            <GoLocation className="h-8 w-22pxr" />
-            밋업
-          </Link>
-        </li>
-        <li className="cursor-pointer text-contentTertiary hover:text-primary">
-          <Link to="profile" className="flex flex-col items-center">
-            <GoPerson className="h-8 w-22pxr" />
-            프로필
-          </Link>
-        </li>
+    <div>
+      <ul className="flex items-center justify-between px-5 pb-3 pt-10pxr shadow-navShadow">
+        <NavBarItem togo={'home'} icon={GoHome} label={'홈'} />
+        <NavBarItem togo={'exchange'} icon={GoArrowSwitch} label={'교환'} />
+        <NavBarItem togo={'meetup'} icon={GoLocation} label={'밋업'} />
+        <NavBarItem togo={'profile'} icon={GoPerson} label={'프로필'} />
       </ul>
     </div>
   );
