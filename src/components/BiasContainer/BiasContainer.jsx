c2be74750426854a63dd4d46ebcebaa7fa40e47c
song0331 <thdgusrb@gmail.com>
2024-02-28 15:07:15 +0900

    [modify] BiasContainer 컴포넌트 css 수정

--- src/components/BiasContainer/BiasContainer.jsx
+++ src/components/BiasContainer/BiasContainer.jsx
@@ -2,7 +2,7 @@ import Bias from '../Bias/Bias';
 
 export default function BiasContainer() {
   return (
-    <div className="biasContainer bg-primary flex items-center gap-3 w-[360px] h-[100px] px-4 overflow-x-scroll">
+    <div className="draggable bg-[#D8D6FF] flex items-center w-[360px] h-[100px]">
       <Bias />
     </div>
   );
