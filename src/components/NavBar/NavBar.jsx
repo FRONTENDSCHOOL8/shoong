01ece012a1e331a868d24a10f865f772efcd0cfc
KIMCHOONGMAN <cm730712@gmail.com>
2024-02-28 14:00:47 +0900

    [Modify] NavBarItem map()으로 구현

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,25 @@
 import { GoArrowSwitch, GoPerson, GoLocation, GoHome } from 'react-icons/go';
 import NavBarItem from './NavBarItem';
+import { useId } from 'react';
 
 export default function NavBar() {
+  const navItems = [
+    { id: useId(), togo: 'home', icon: GoHome, label: '홈' },
+    { id: useId(), togo: 'exchange', icon: GoArrowSwitch, label: '교환' },
+    { id: useId(), togo: 'meetup', icon: GoLocation, label: '밋업' },
+    { id: useId(), togo: 'profile', icon: GoPerson, label: '프로필' },
+  ];
   return (
     <div>
       <ul className="flex items-center justify-between px-5 pb-3 pt-10pxr shadow-navShadow">
-        <NavBarItem togo={'home'} icon={GoHome} label={'홈'} />
-        <NavBarItem togo={'exchange'} icon={GoArrowSwitch} label={'교환'} />
-        <NavBarItem togo={'meetup'} icon={GoLocation} label={'밋업'} />
-        <NavBarItem togo={'profile'} icon={GoPerson} label={'프로필'} />
+        {navItems.map((item) => (
+          <NavBarItem
+            key={item.id}
+            togo={item.togo}
+            icon={item.icon}
+            label={item.label}
+          />
+        ))}
       </ul>
     </div>
   );
