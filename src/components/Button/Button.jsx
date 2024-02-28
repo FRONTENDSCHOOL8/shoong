378516c129e4a44d6b5c61dc95eac8bf19ef4bf0
highballplz <kmkim405@gmail.com>
2024-02-28 11:50:45 +0900

    [Feat] 버튼 컴포넌트 완성

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,1 +1,35 @@
 // 버튼 컴포넌트
+export default function Button({ small, disabled, children }) {
+  let width, height, backgroundColor, fontWeight, paddingX, paddingY;
+  if (small) {
+    (width = 'w-[69px]'),
+      (height = 'h-7'),
+      (fontWeight = 'medium'),
+      (paddingX = 'px-2.5'),
+      (paddingY = 'py-1');
+  } else {
+    (width = 'w-[265px]'),
+      (height = 'h-11'),
+      (fontWeight = 'semibold'),
+      (paddingX = 'px-[91px]'),
+      (paddingY = 'py-3');
+  }
+
+  if (disabled) {
+    backgroundColor = 'bg-gray-400';
+  } else {
+    backgroundColor = 'bg-indigo-500';
+  }
+
+  return (
+    <button
+      className={`${width} ${height} ${paddingX} ${paddingY} ${backgroundColor} rounded-[10px] justify-center items-center gap-2.5 inline-flex`}
+    >
+      <div
+        className={`text-white text-sm ${fontWeight} font-['SUIT Variable'] leading-tight`}
+      >
+        {children}
+      </div>
+    </button>
+  );
+}
