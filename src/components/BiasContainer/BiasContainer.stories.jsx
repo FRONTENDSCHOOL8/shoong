a7ede0ed8731be3bec38958cc3501ef467c97455
song0331 <thdgusrb@gmail.com>
2024-02-29 20:29:42 +0900

    [add] Bias, BiasContinaer 컴포넌트 스토리북 추가

---
+++ src/components/BiasContainer/BiasContainer.stories.jsx
@@ -0,0 +1,9 @@
+import BiasContainer from './BiasContainer';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: BiasContainer,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const Default = {};
