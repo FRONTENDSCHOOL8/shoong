6709fe9eb803bf1563196310b816cf5d6b65548b
pistapixie <hleehslee@gmail.com>
2024-03-01 20:09:15 +0900

    [Modify] PhocaImg 컴포넌트 props 이름 변경

--- src/components/PhocaItem/PhocaImg.jsx
+++ src/components/PhocaItem/PhocaImg.jsx
@@ -1,12 +1,12 @@
 import PhocaLikeButton from './PhocaLikeButton';
 
-export default function PhocaImg({ src, alt }) {
+export default function PhocaImg({ phocaImgSrc, phocaImgAlt }) {
   return (
     <div className="mb-4 min-w-176pxr relative">
       <img
-        src={src}
+        src={phocaImgSrc}
         className="h-full w-full object-cover rounded-xl"
-        alt={alt}
+        alt={phocaImgAlt}
       />
       <div className="absolute bottom-3 right-4">
         <PhocaLikeButton />
--- src/components/PhocaItem/PhocaItem.jsx
+++ src/components/PhocaItem/PhocaItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PhocaImg from './PhocaImg';
+import ArtistLogo from './ArtistLogo';
 import ArtistInfo from './ArtistInfo';
 import PhocaTitle from './PhocaTitle';
+import PhocaLikeCount from './PhocaLikeCount';
 import { Link } from 'react-router-dom';
 // import { Link, redirect, useOutletContext } from 'react-router-dom';
-import ArtistLogo from './ArtistLogo';
-import PhocaLikeCount from './PhocaLikeCount';
 
 import pb from '@/api/pocketbase';
 import { useState, useEffect } from 'react';
@@ -33,25 +33,32 @@ export default function PhocaItem({ title, altText, likes, imgUrl }) {
   }, []);
 
   return (
-    <li
-      className="list-none m-0 p-0 w-44 h-[353px] relative"
-      style={{ width: '11rem', height: '22rem' }}
-    >
-      <a
-        to="/"
-        aria-label={`${title} 카드 디테일 페이지로 이동`}
-        className="flex flex-col"
-      >
-        <PhocaImg />
-        <div className="flex gap-2">
-          <ArtistLogo imgUrl={imgUrl} altText={altText} />
-          <ArtistInfo />
-        </div>
-        <div className="flex flex-col items-start">
-          <PhocaTitle title={title} />
-          <PhocaLikeCount likes={likes} />
-        </div>
-      </a>
-    </li>
+    <ul>
+      {phocaImg.map((item, index) => {
+        return (
+          <li
+            key={index}
+            className="list-none m-0 p-0 w-44 h-[353px] relative"
+            style={{ width: '11rem', height: '22rem' }}
+          >
+            <a
+              to="/"
+              aria-label={`${title} 카드 디테일 페이지로 이동`}
+              className="flex flex-col"
+            >
+              <PhocaImg />
+              <div className="flex gap-2">
+                <ArtistLogo imgUrl={imgUrl} altText={altText} />
+                <ArtistInfo />
+              </div>
+              <div className="flex flex-col items-start">
+                <PhocaTitle title={title} />
+                <PhocaLikeCount likes={likes} />
+              </div>
+            </a>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
