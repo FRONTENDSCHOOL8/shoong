51f177ed9ecdafa832fac6b399a027154f59d7ca
pistapixie <hleehslee@gmail.com>
2024-03-01 19:36:39 +0900

    [Modify] ArtistInfo 컴포넌트에 props 추가

--- src/components/PhocaItem/ArtistInfo.jsx
+++ src/components/PhocaItem/ArtistInfo.jsx
@@ -1,8 +1,8 @@
-export default function ArtistInfo() {
+export default function ArtistInfo({ groupName, artistName }) {
   return (
     <div className="flex flex-col items-start">
-      <p className="text-m04 font-m04 text-contentSecondary">그룹명</p>
-      <p className="text-r01 font-r01 text-contentSecondary">아티스트명</p>
+      <p className="text-m04 font-m04 text-contentSecondary">{groupName}</p>
+      <p className="text-r01 font-r01 text-contentSecondary">{artistName}</p>
     </div>
   );
 }
