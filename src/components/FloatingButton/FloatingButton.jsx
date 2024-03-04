0f2e4bede97858bebb6b17543d3bc03f7ecb504b
roben31380 <kinalee0430@gmail.com>
2024-03-04 09:50:28 +0900

    [Feat] Floating button 마크업 및 스타일링 - 마크업 및 스타일링 - useProfileImage 적용

--- src/components/FloatingButton/FloatingButton.jsx
+++ src/components/FloatingButton/FloatingButton.jsx
@@ -1,36 +1,21 @@
+import { useState } from 'react';
+import useProfileImage from './useProfileImage';
+
 export default function FloatingButton() {
+  const [isLogIn] = useState(true);
+  const profileImage = useProfileImage(isLogIn);
+  // const handleFloatingButton = () => {};
   return (
-    <button type="button" className="absolute bottom-0 right-0">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={60}
-        height={60}
-        viewBox="0 0 60 60"
-        fill="none"
-      >
-        <circle
-          cx="29"
-          cy="29"
-          r="25"
-          strokeWidth="2"
-          fill="white"
-          stroke="url(#paint0_linear_7194_2228)"
-        />
-        <image width={35} x={10} y={13} href="/icons/heartLogo.png" />
-        <defs>
-          <linearGradient
-            id="paint0_linear_7194_2228"
-            x1="29.5"
-            y1={0}
-            x2="29.5"
-            y2={59}
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#FE5F5F" />
-            <stop offset={1} stopColor="#6662C9" />
-          </linearGradient>
-        </defs>
-      </svg>
+    <button
+      type="button"
+      aria-label="찜목록으로 가기"
+      // onClick={() => handleFloatingButton}
+      className="absolute bottom-5 right-5 w-59pxr h-59pxr bg-gradient-to-b from-red-400 to-indigo-500 rounded-full shadow-lg"
+    >
+      <img
+        className="w-55pxr h-55pxr rounded-full absolute left-2pxr top-2pxr"
+        src={profileImage}
+      />
     </button>
   );
 }
