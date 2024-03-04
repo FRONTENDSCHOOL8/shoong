9372f20059a4997d04e87b9bee13b57bdfb35c6b
roben31380 <kinalee0430@gmail.com>
2024-03-04 17:59:46 +0900

    [Feat] searchBar 컴포넌트 완성

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,20 +2,26 @@ import { debounce } from 'lodash';
 import { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 
-export default function SearchBar() {
+/**
+ * @param {string} props.name
+ * @param {string} props.placeholder
+ * @returns
+ */
+export default function SearchBar({ name, placeholder, bgStyle }) {
   const [search, setSearch] = useState('');
   const handleSearch = (e) => {
     setSearch(e.target.value);
   };
-
   return (
-    <form className="py-1.5 px-4 bg-zinc-100 rounded-[30px] flex-row justify-start items-start gap-2 inline-flex">
+    <form
+      className={`${bgStyle} py-1.5 px-4 rounded-[30px] flex-row justify-start items-start gap-2 inline-flex`}
+    >
       <BsSearch className="h-6" />
-      <label htmlFor=""></label>
+      <label htmlFor={name}></label>
       <input
         type="text"
-        name=""
-        placeholder="placeholder"
+        name={name}
+        placeholder={placeholder}
         className="bg-transparent"
         onChange={debounce(handleSearch, 500)}
         defaultValue={search}
