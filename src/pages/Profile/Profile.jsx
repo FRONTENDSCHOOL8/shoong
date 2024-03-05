ab34310923d66ea803051a1df4eae3321aa6adfa
kina <148828856+roben31380@users.noreply.github.com>
2024-03-06 01:23:03 +0900

    Feat/profile header (#35)
    * [Feat] profile loader 작성
    - userData를 가져오는 function 작성 및 연결
    
    * [Feat] profile title 마크업 및 스타일링
    
    * [Feat] ProfileHeader 마크업 및 스타일링
    - profileTtitle + profileInfo를 ProfileHeader로 묶어서 작업함
    
    * [Feat] profileHeader 구현

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,8 @@
-
+import ProfileHeader from "@/components/ProfileTitle/ProfileHeader";
 
 
 export default function Profile() {
-  return <div>Profile
-
+  return <div className="min-w-360pxr">
+    <ProfileHeader/>
   </div>;
 }
