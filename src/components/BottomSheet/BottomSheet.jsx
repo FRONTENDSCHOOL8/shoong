7e0cbb9419a1d6f4813d5676b623f44caf49f7ed
highballplz <kmkim405@gmail.com>
2024-03-05 16:29:09 +0900

    [Feat] BottomSheet 리스트 렌더링

--- src/components/BottomSheet/BottomSheet.jsx
+++ src/components/BottomSheet/BottomSheet.jsx
@@ -1,20 +1,18 @@
 import React, { useId } from 'react';
 import Button from '../Button/Button';
 
-export default function BottomSheetRadio({ itemCount, itemList }) {
+export default function BottomSheetRadio({ itemList }) {
   return (
     // 왜 rounded에는 pxr 적용이 안 되지?
     <>
       <div className="flex h-296pxr flex-col items-center rounded-tl-[30px] rounded-tr-[30px] bg-indigo-200">
         <div className="mb-12pxr mt-10pxr h-5pxr w-38pxr rounded-[5px] bg-zinc-100" />
-        <RadioItem>hello</RadioItem>
-        <BorderLine />
-        <RadioItem>hello</RadioItem>
-        <BorderLine />
-        <RadioItem>hello</RadioItem>
-        <BorderLine />
-        <RadioItem>hello</RadioItem>
-        <BorderLine />
+        {itemList.map((item, index) => (
+          <div className="w-10/12" key={index}>
+            <RadioItem>{item}</RadioItem>
+            {index < itemList.length - 1 && <BorderLine />}
+          </div>
+        ))}
         <Button style="mt-16pxr">확인</Button>
       </div>
     </>
@@ -23,7 +21,7 @@ export default function BottomSheetRadio({ itemCount, itemList }) {
 
 function RadioItem({ children }) {
   return (
-    <label className="my-12pxr flex w-9/12 flex-row gap-12pxr">
+    <label className="my-12pxr flex flex-row gap-16pxr pl-16pxr">
       <input type="radio" />
       <span className="font-semiboldtext-neutral-600 grow text-left text-sm">
         {children}
@@ -33,5 +31,5 @@ function RadioItem({ children }) {
 }
 
 function BorderLine() {
-  return <div className="h-1pxr w-10/12 bg-zinc-100"></div>;
+  return <div className="h-1pxr w-full bg-zinc-100"></div>;
 }
