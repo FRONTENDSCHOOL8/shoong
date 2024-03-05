7ab0533c409a18d4c9c292f382013e010dc539f9
highballplz <kmkim405@gmail.com>
2024-03-06 03:51:57 +0900

    [Feat] useBottomSheet 커스텀 훅에 bottomSheet 애니메이션 구현

--- src/components/BottomSheet/useBottomSheet.jsx
+++ src/components/BottomSheet/useBottomSheet.jsx
@@ -4,15 +4,28 @@ export default function useBottomSheet() {
   const [isOpen, setIsOpen] = useState(false);
   const button = useRef(null);
   const bottomSheet = useRef(null);
+  console.log(button.current);
 
   useEffect(() => {
     const handleBottomSheet = (e) => {
       setIsOpen((isOpen) => !isOpen);
-      bottomSheet.current.style.display = isOpen ? 'block' : 'none';
     };
+
     button.current.addEventListener('click', handleBottomSheet);
+
+    bottomSheet.current.className =
+      'transition-all duration-500 ease-in-out absolute w-full left-0';
+
+    if (isOpen) {
+      bottomSheet.current.style.bottom = '0px';
+    } else {
+      bottomSheet.current.style.bottom = `-${getComputedStyle(bottomSheet.current).height}`;
+    }
+
+    //eslint가 이렇게 변수에 담으라고 해서 담았는데 왜 이렇게 해줘야 되는지는 모르겠음..
+    const current = button.current;
     return function cleanUp() {
-      button.current.removeEventListener('click', handleBottomSheet);
+      current.removeEventListener('click', handleBottomSheet);
     };
   });
 
