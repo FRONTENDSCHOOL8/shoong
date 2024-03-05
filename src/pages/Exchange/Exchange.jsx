5bcea7801b1108a58de5bde927d417ed94f71dce
kina <148828856+roben31380@users.noreply.github.com>
2024-03-05 15:19:03 +0900

    [Feat]search bar 구현 (#31)
    * [Feat] 기본 searchBar 컴포넌트 구현
    
    * [Feat] searchBar 컴포넌트 완성
    
    * [Feat] searchBar의 text를 전역에 등록
    
    ---------
    
    Co-authored-by: 송현규 <108565747+song0331@users.noreply.github.com>

--- src/pages/Exchange/Exchange.jsx
+++ src/pages/Exchange/Exchange.jsx
@@ -1,3 +1,10 @@
+import SearchBar from '@/components/SearchBar/SearchBar';
+
 export default function Exchange() {
-  return <div>Exchange</div>;
+  return (
+    <div>
+      Exchange
+      <SearchBar name="Exchange" placeholder="포카찾기" bgStyle="bg-zinc-100" />
+    </div>
+  );
 }
