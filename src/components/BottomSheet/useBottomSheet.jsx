bb418ff0ec09d9bd583184d6d988f3f782266a13
highballplz <kmkim405@gmail.com>
2024-03-05 11:19:36 +0900

    [Feat] useBottomSheet 커스텀 훅 완성

---
+++ src/components/BottomSheet/useBottomSheet.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+export default function useBottomSheet() {
+  const [isOpen, setIsOpen] = useState(false);
+  const button = useRef(null);
+  const bottomSheet = useRef(null);
+  useEffect(() => {
+    const handleBottomSheet = (e) => {
+      setIsOpen((isOpen) => !isOpen);
+      bottomSheet.current.style.display = isOpen ? 'block' : 'none';
+    };
+    button.current.addEventListener('click', handleBottomSheet);
+    return function cleanUp() {
+      button.current.removeEventListener('click', handleBottomSheet);
+    };
+  });
+  return { button, bottomSheet };
+}
