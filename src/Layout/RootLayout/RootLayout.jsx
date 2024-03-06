afbf136d84d244b1e8052bd186e12dafaaaac58c
KIMCHOONGMAN <cm730712@gmail.com>
2024-03-06 14:33:56 +0900

    [Modify] NavBar 하단 고정 및 RootLayout폴더로 폴더 위치 변경

--- src/Layout/RootLayout/RootLayout.jsx
+++ src/Layout/RootLayout/RootLayout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import NavBar from '@/components/NavBar/NavBar';
+import NavBar from '../NavBar/NavBar';
 
 export default function RootLayout() {
   return (
     <>
       <Header />
-      <Outlet />
-      <NavBar />
+      <div className="pb-10">
+        <Outlet />
+      </div>
       <Footer />
+      <NavBar />
     </>
   );
 }
