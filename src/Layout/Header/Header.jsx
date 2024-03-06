9ede43ebf37d551914fec9895790356f1f25c7d3
highballplz <118330822+highballplz@users.noreply.github.com>
2024-03-06 11:11:10 +0900

    [Modify] Header에 들어가는 로고의 경로를 절대경로로 수정 (#39)

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className="flex h-55pxr w-full justify-between bg-white ">
       <Link to="./">
-        <img className="flex h-36pxr w-100pxr" src="../icons/shoongLogo.svg" />
+        <img className="flex h-36pxr w-100pxr" src="/icons/shoongLogo.svg" />
       </Link>
       <div className="flex gap-20pxr">
         <Link to="/chatting">
