6f5a650e1d84a111748220ae15d3816469ab9986
song0331 <thdgusrb@gmail.com>
2024-03-08 16:22:21 +0900

    [modify] 드볼 이미지 추가, 스타일링 수정

--- src/components/DragonSphere/DragonSphere.jsx
+++ src/components/DragonSphere/DragonSphere.jsx
@@ -4,15 +4,17 @@ export default function DragonSphere({ phocaInfo, phocaData, group }) {
   return (
     <>
       <div className="absolute bottom-5 left-[50%] z-10 flex h-100pxr w-280pxr translate-x-[-50%] rounded-[10px] bg-white opacity-95 shadow">
-        <div className="m-auto flex items-center gap-4">
-          <div className="h-65pxr w-65pxr">
+        <div className="m-auto flex items-center gap-8">
+          <div className="relative">
             <Circle
               percent={Math.round((phocaInfo.length / phocaData.length) * 100)}
-              strokeWidth={25}
+              strokeWidth={15}
               trailColor="#DCD6FA"
-              trailWidth={18}
+              trailWidth={13}
               strokeColor="#6662C9"
+              className="absolute -left-5 -top-4 z-10 h-65pxr w-65pxr"
             />
+            <img src="/icons/드래곤볼.jpg" alt="드볼 이미지" />
           </div>
 
           <div className="inline-flex flex-col items-start justify-start gap-px">
