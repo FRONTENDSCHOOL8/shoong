32e1b40c9089f4be70a31ab90d85a90b882156f6
pistapixie <hleehslee@gmail.com>
2024-03-11 08:15:42 +0900

    [Feat] ProfileSetting 페이지 구현

--- src/pages/ProfileSetting/ProfileSetting.jsx
+++ src/pages/ProfileSetting/ProfileSetting.jsx
@@ -1,7 +1,29 @@
+import NavigationTile from '@/components/NavigationTile/NavigationTile';
+// import DetailHeader from '@/components/DetailHeader/DetailHeader';
+
 export default function ProfileSetting() {
   return (
     <div>
-      <div></div>
+      {/* <DetailHeader>설정</DetailHeader> */}
+      <div>
+        <span className="mb-4 mt-2 block px-6 pt-4 text-lg font-bold">
+          내 정보
+        </span>
+        <NavigationTile to="/" text="로그인 정보" className="" />
+        <NavigationTile to="/" text="회원 정보 수정" className=" " />
+        <NavigationTile to="/" text="콜렉트북" className=" " />
+        <NavigationTile to="/" text="교환현황" className=" " />
+        <NavigationTile to="/" text="알림 설정" className=" " />
+      </div>
+      <div>
+        <span className="mb-4 mt-20 block px-6 text-lg font-bold">
+          서비스 정보
+        </span>
+        <NavigationTile to="/" text="서비스 이용약관" className=" " />
+        <NavigationTile to="/" text="개인정보 처리방침" className=" " />
+        <NavigationTile to="/" text="언어" className=" " />
+        <NavigationTile to="/" text="버전 정보" className=" " />
+      </div>
     </div>
   );
 }
