d75353444612528231984fc41bd086d3ea78f4e2
highballplz <kmkim405@gmail.com>
2024-03-11 13:41:39 +0900

    [Modify] bottomSheet div 클래스명에 radio/text 중 하나인 layout을 넣어서 isOpen 상태 됐을 때 각각의 레이아웃에 맞는 높이만큼 올라오게 하기

--- src/components/BottomSheet/BottomSheet.jsx
+++ src/components/BottomSheet/BottomSheet.jsx
@@ -5,7 +5,8 @@ import BottomSheetAnimation2 from './BottomSheetAnimation2';
 
 export default function BottomSheet({
   itemList = [],
-  radio = false,
+  isRadio = false,
+  text = '',
   children,
   linkedBottomSheet,
   isOpen,
@@ -25,14 +26,16 @@ export default function BottomSheet({
       checkedName={checkedName}
     />
   );
-  const textLayout = <TextLayout>{children}</TextLayout>;
+  const textLayout = <TextLayout text={text} />;
+
+  const layout = isRadio ? 'radio' : 'text';
 
   return (
     // 왜 rounded에는 pxr 적용이 안 되지?
     <div className="upSensor bg-transparent">
       <div
         id={'bottomSheet'}
-        className={`fixed bottom-0 flex w-full flex-col items-center rounded-tl-[30px] rounded-tr-[30px] bg-indigo-200 duration-1000`}
+        className={`${layout} fixed bottom-0 flex w-full flex-col items-center rounded-tl-[30px] rounded-tr-[30px] bg-indigo-200 duration-1000`}
         ref={linkedBottomSheet}
         onMouseDown={handleStart}
         onMouseMove={handleMove}
@@ -40,7 +43,7 @@ export default function BottomSheet({
         <div className="bottomSheetHandleWrap flex w-10/12 justify-center">
           <div className="bottomSheetHandle mb-12pxr mt-10pxr h-5pxr w-38pxr rounded-[5px] bg-zinc-100" />
         </div>
-        {radio ? radioLayout : textLayout}
+        {isRadio ? radioLayout : textLayout}
       </div>
     </div>
   );
