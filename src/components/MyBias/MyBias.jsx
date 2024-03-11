6fab9bb30a3ff618001ab1ed4d9cb7ae00eb0b6b
KIMCHOONGMAN <cm730712@gmail.com>
2024-03-11 22:57:57 +0900

    [Modify] vercel 배포 후 에러 수정

--- src/components/MyBias/MyBias.jsx
+++ src/components/MyBias/MyBias.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function MyBias() {
@@ -9,7 +8,7 @@ export default function MyBias() {
         className="flex h-11 w-11 items-center justify-center rounded-full bg-gradient-to-b from-red-400 to-indigo-500"
       >
         <img
-          src="../../../public/myBias.jpg"
+          src="/myBias.jpg"
           alt="내 최애 선택하기"
           className="m-auto h-10 w-10 cursor-pointer rounded-full"
         />
