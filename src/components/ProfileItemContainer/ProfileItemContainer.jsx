28a8789628256352e56bfef1b333dfbee507e4ed
song0331 <thdgusrb@gmail.com>
2024-03-12 01:02:27 +0900

    [Modify] ProfileItemContainer 컴포넌트 패딩 수정

--- src/components/ProfileItemContainer/ProfileItemContainer.jsx
+++ src/components/ProfileItemContainer/ProfileItemContainer.jsx
@@ -12,7 +12,7 @@ import { FaRegAddressBook } from 'react-icons/fa6';
 export default function ProfileItemContainer({ title, children }) {
   return (
     <>
-      <div className="mx-20pxr h-52 rounded-[10px] bg-white bg-opacity-70 shadow">
+      <div className="mx-20pxr h-52 rounded-[10px] bg-white bg-opacity-70 pb-10pxr shadow">
         <div className="font-semibold-40pxr flex items-center gap-1 p-3 text-gray-500">
           <FaRegAddressBook />
           <span>{title}</span>
