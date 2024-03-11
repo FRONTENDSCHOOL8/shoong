54e4060c0066990ddc45212f1e771aba56dfd2b4
highballplz <kmkim405@gmail.com>
2024-03-12 06:01:08 +0900

    [Modify] RootLayout에서 Outlet에 헤더 높이만큼 pt 줌

--- src/Layout/RootLayout/RootLayout.jsx
+++ src/Layout/RootLayout/RootLayout.jsx
@@ -7,7 +7,7 @@ export default function RootLayout() {
   return (
     <>
       <Header />
-      <div className="pb-14">
+      <div className="pb-14 pt-55pxr">
         <Outlet />
       </div>
       <Footer />
