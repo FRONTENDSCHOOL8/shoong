562b26886d3a38e8cbd01aeaf89068ba31721fb5
KIMCHOONGMAN <cm730712@gmail.com>
2024-03-12 16:04:51 +0900

    [Feat] meetUp 페이지일 경우 Footer null 처리

--- src/Layout/RootLayout/RootLayout.jsx
+++ src/Layout/RootLayout/RootLayout.jsx
@@ -2,15 +2,17 @@ import { Outlet } from 'react-router';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
+import { useLocation } from 'react-router-dom';
 
 export default function RootLayout() {
+  const { pathname } = useLocation();
+
+  const isHiddenFooter = pathname === '/meetup';
   return (
     <>
       <Header />
-      <div className="py-68pxr">
-        <Outlet />
-      </div>
-      <Footer />
+      <Outlet />
+      {isHiddenFooter ? null : <Footer />}
       <NavBar />
     </>
   );
