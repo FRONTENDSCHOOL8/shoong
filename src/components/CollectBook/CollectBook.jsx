9eea40b14b71741fe819773bf84aeb19f334bad2
송현규 <108565747+song0331@users.noreply.github.com>
2024-03-13 01:12:13 +0900

    [Feat] 로그인 관련 기능 (#78)
    * [Modify] 잠시 유저 정보 제거
    
    * [Feat] 로그인 유무 전역으로 관리
    
    * [Feat] NotFound 컴포넌트 추가
    
    * [Feat] 콜렉트북 컴포넌트 추가
    
    * [Modify] 로그인 관련 기능 구현

--- src/components/CollectBook/CollectBook.jsx
+++ src/components/CollectBook/CollectBook.jsx
@@ -1,4 +1,4 @@
-import pb from '@/api/pocketbase';
+import { isLogin } from '@/store/store';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { FaPlus } from 'react-icons/fa6';
@@ -6,18 +6,11 @@ import { Link } from 'react-router-dom';
 
 export default function CollectBook() {
   const [book, setBook] = useState([]);
+  const { collectBook } = isLogin();
 
   useEffect(() => {
-    const authData = pb
-      .collection('users')
-      .authWithPassword('song', '123456789', { expand: 'collectBook' });
-    pb.autoCancellation(false);
-
-    authData.then((res) => {
-      const collectBook = res.record.expand.collectBook;
-      setBook(collectBook);
-    });
-  }, []);
+    setBook(collectBook);
+  }, [collectBook]);
 
   return (
     <>
@@ -27,20 +20,20 @@ export default function CollectBook() {
               return (
                 <li
                   key={item.id}
-                  className="flex h-full w-100pxr cursor-pointer items-center justify-center overflow-hidden rounded-[5px] bg-zinc-300"
+                  className="flex h-135pxr w-100pxr cursor-pointer items-center justify-center overflow-hidden rounded-[5px] bg-zinc-300"
                 >
                   <Link to={`/collectBook/${item.group}/${item.id}`}>
                     <img
                       className="h-full w-full rounded object-cover"
                       src={`https://shoong.pockethost.io/api/files/collectBook/${item.id}/${item.thumbNail}`}
-                      alt={item.title}
+                      alt="콜렉트북 커버 사진"
                     />
                   </Link>
                 </li>
               );
             })
           : null}
-        <li className="flex h-full w-100pxr cursor-pointer items-center justify-center rounded-[5px] bg-zinc-300">
+        <li className="flex h-135pxr w-100pxr cursor-pointer items-center justify-center rounded-[5px] bg-zinc-300">
           <Link to="/profile">
             <FaPlus className="text-zinc-600" />
           </Link>
