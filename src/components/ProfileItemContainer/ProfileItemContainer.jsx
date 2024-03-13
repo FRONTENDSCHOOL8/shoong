cfc9b492df079ec9580a654adc7211205e0f31df
pistapixie <144419094+pistapixie@users.noreply.github.com>
2024-03-14 00:46:58 +0900

    [Modify] 프로필 페이지 css 수정 (#96)
    * [Feat]PhocaItem 컴포넌트 마크업 완성
    
    * [Feat]PhocaItem 스타일링 완성
    
    * [Modify] PhocaImg 컴포넌트 수정
    
    * [Modify] PhocaImg 컴포넌트에 props 추가
    
    * [Modify] ArtistInfo 컴포넌트에 props 추가
    
    * [Modify] PhocaImg 컴포넌트 props 이름 변경
    
    * [Modify] ArtistLogo 컴포넌트 props명 수정
    
    * [Feat] 그룹 로고 이미지 리스트 랜더링
    
    * [Modify] PhocaItem 컴포넌트 수정
    
    * [Feat] PhocaItem 컴포넌트 완성
    
    * [Feat] PhocaContainer 스크롤 기능 구현
    
    * [Feat] PhocaContainer 컴포넌트 리스트 랜더링 기능 구현 확인
    
    * [Bugfix] PhocaItem 컴포넌트 JsDoc 수정
    
    * [Modify] PhocaImg 동적 클래스 및 children 추가 변경
    
    * [Modify] PhocaItem 하위 컴포넌트들 className props로 분리
    
    * [Modify] PhocaContainer 컴포넌트 JsDoc 수정
    
    * [Modify] 홈 페이지에 랜더링되는 PhocaItem 스타일링 수정
    
    * [Feat] 교환 페이지 PhocaContainerEx 기능 구현
    
    * [Feat]PhocaContainer 랜더링 구현
    
    * [Feat] 포카 데이터 로딩 로직을 커스텀 훅으로 분리
    
    - 좋아요 수와 생성 날짜 기준으로 포카 데이터를 가져오고 정렬하는 `usePhocaDataByLikeCount` 및 `usePhocaDataByCreated` 훅 구현
    - 데이터 가져오기를 간소화하고 코드 가독성을 향상시키기 위해 `PhocaContainer` 컴포넌트에서 커스텀 훅 사용
    - 컴포넌트에서 직접 `useLoaderData` 호출을 제거하고, 데이터 가져오기를 훅에 캡슐화하여 재사용성 및 관심사의 분리 향상
    
    * [Feat] NavigationTile 컴포넌트 마크업
    
    * [Feat] Footer, ProfileFooter 구현 완료
    
    * [Modify] Footer 컴포넌트 스타일링 수정
    
    * [Feat] PhocaContainerEx 수정 - sorting bar 추가
    
    * [Modify] 프로필 페이지 css 수정
    
    * [Feat] ProfileSetting 페이지 구현
    
    * [Feat] LoginInfo 마크업, 스타일링
    
    * [Modify] ProfileSetting 마크업 수정
    
    * [Feat] MyBias 경로 설정
    
    * [Feat] 최애 선택 페이지 마크업, 스타일링
    
    * [Feat] userEmail, userName 데이터 랜더링
    
    * [Feat] PhocaLikeButton 토글 기능 구현 및 이벤트 버블링 에러 fix
    
    * [Modify] 로그아웃 버튼 스타일 변경
    
    * [Modify] 주석 수정
    
    * [Modify] bias 스타일링 수정
    
    * [Modify] public 경로 수정
    
    * [Modify] PhocaLikeButton userData 추가
    
    * [Feat] PhpcaLikeButton 클릭시 유저의 likeList에 업데이트 기능 구현
    
    * [Feat] PhocaLikeButton 기능 구현 완료 - 찜 개수 업데이트, 찜 카드 목록 업데이트
    
    * [Feat]LikeDetail 컴포넌트 마크업
    
    * [Feat] LikeDetail 컴포넌트 기능 구현
    
    * [Modify] LikeDetail 컴포넌트 css 수정
    
    * [Modify] 주석 삭제
    
    * [Modify] bias 컴포넌트 스타일 수정
    
    * [Modify] BiasContainer 스타일 수정
    
    * [Modify] 포카 컨테이너 마진 수정
    
    * [Modify]Footer 마진 변경
    
    * [Modify] LogInfo 스타일 변경
    
    * [Feat] 로그아웃, 서비스 탈퇴 기능 구현
    
    * [Modify] MyBias 스타일 수정
    
    * [Modify] 최애 선택 페이지 스타일 변경
    
    * [Feat] 플로팅 버튼에 페이지 연결
    
    * [Modify] MyBias 기능 수정
    
    * [Feat].env 추가
    
    * [Modify] bg 그라디언트 변경
    
    * [Modify] 교환 디테일 페이지 ui 수정
    
    * [Modify] ProfileTitle ui 수정
    
    * [Modify] Profile 페이지 ui 수정
    
    * [Modify] Profile 페이지 css 수정
    
    * [Modify] MyBias 페이지 css 수정
    
    * [Modify] css 수정

--- src/components/ProfileItemContainer/ProfileItemContainer.jsx
+++ src/components/ProfileItemContainer/ProfileItemContainer.jsx
@@ -12,10 +12,10 @@ import { FaRegAddressBook, FaAngleRight } from 'react-icons/fa6';
 export default function ProfileItemContainer({ title, children }) {
   return (
     <>
-      <div className="mx-20pxr flex h-52 flex-col rounded-[10px] bg-white bg-opacity-70 pb-10pxr shadow">
-        <div className="font-semibold-40pxr flex items-center gap-1 p-3 text-gray-500">
+      <div className="h-50 mx-20pxr flex flex-col rounded-[10px] bg-white bg-opacity-70 pb-10pxr shadow-lg">
+        <div className="flex items-center gap-1 p-3 text-18pxr font-m01 text-gray-700">
           <FaRegAddressBook />
-          <span>{title}</span>
+          <span className="">{title}</span>
           <FaAngleRight className="ml-auto" />
         </div>
 
