f36dc2970cca0fb21213fbc4c580916165a79b77
roben31380 <kinalee0430@gmail.com>
2024-03-13 09:23:56 +0900

    [Modify] 댓글아이템 스타일링 수정

--- src/components/Comment/CommentItem.jsx
+++ src/components/Comment/CommentItem.jsx
@@ -1,5 +1,5 @@
 import pb from '@/api/pocketbase';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useId } from 'react';
 
 export default function CommentItem({ id, ment, date }) {
   const [writer, setWriter] = useState({});
@@ -15,16 +15,16 @@ export default function CommentItem({ id, ment, date }) {
   const src = `https://shoong.pockethost.io/api/files/users/${writer.id}/${writer.avatar}`;
   return (
     <article
-      key={id} // Add a unique key for each comment
-      className="my-4 flex flex-row items-center gap-x-4 rounded-xl bg-white p-2 shadow-lg"
+      key={useId()}
+      className="my-4 flex flex-row items-center gap-x-4 rounded-xl bg-white p-2 shadow-lg transition-all duration-1000"
     >
-      <img src={src} className="aspect-square h-42pxr rounded-full" />
+      <img src={src} className="aspect-square h-42pxr rounded-full shadow-md" />
       <div className="flex flex-col flex-nowrap gap-x-1 ">
-        <p className="text-left text-base font-bold">
+        <p className="text-left text-sb02 font-sb02 text-contentPrimary">
           {writer.name}
           <span className="px-1 text-m01 text-contentSecondary">{newDate}</span>
         </p>
-        <p className="text-left">{ment}</p>
+        <p className="text-left text-r01">{ment}</p>
       </div>
     </article>
   );
