a2a79c206674ca12d66b2e4e8da4bcc72be46c7b
pistapixie <144419094+pistapixie@users.noreply.github.com>
2024-03-13 10:06:58 +0900

    [Feat]  (#84)
    * [Feat]PhocaItem 컴포넌트 마크업 완성
    
    * [Feat]PhocaItem 스타일링 완성
    
    * [Modify] PhocaImg 컴포넌트 수정
    
    * [Modify] PhocaImg 컴포넌트에 props 추가
    
    * [Modify] ArtistInfo 컴포넌트에 props 추가
    
    * [Modify] PhocaImg 컴포넌트 props 이름 변경
    
    * [Modify] ArtistLogo 컴포넌트 props명 수정
    
    * [Feat] 그룹 로고 이미지 리스트 랜더링
    
    * [Modify] PhocaItem 컴포넌트 수정
    
    * [Feat] PhocaItem 컴포넌트 완성
    
    * [Feat] PhocaContainer 스크롤 기능 구현
    
    * [Feat] PhocaContainer 컴포넌트 리스트 랜더링 기능 구현 확인
    
    * [Bugfix] PhocaItem 컴포넌트 JsDoc 수정
    
    * [Modify] PhocaImg 동적 클래스 및 children 추가 변경
    
    * [Modify] PhocaItem 하위 컴포넌트들 className props로 분리
    
    * [Modify] PhocaContainer 컴포넌트 JsDoc 수정
    
    * [Modify] 홈 페이지에 랜더링되는 PhocaItem 스타일링 수정
    
    * [Feat] 교환 페이지 PhocaContainerEx 기능 구현
    
    * [Feat]PhocaContainer 랜더링 구현
    
    * [Feat] 포카 데이터 로딩 로직을 커스텀 훅으로 분리
    
    - 좋아요 수와 생성 날짜 기준으로 포카 데이터를 가져오고 정렬하는 `usePhocaDataByLikeCount` 및 `usePhocaDataByCreated` 훅 구현
    - 데이터 가져오기를 간소화하고 코드 가독성을 향상시키기 위해 `PhocaContainer` 컴포넌트에서 커스텀 훅 사용
    - 컴포넌트에서 직접 `useLoaderData` 호출을 제거하고, 데이터 가져오기를 훅에 캡슐화하여 재사용성 및 관심사의 분리 향상
    
    * [Feat] NavigationTile 컴포넌트 마크업
    
    * [Feat] Footer, ProfileFooter 구현 완료
    
    * [Modify] Footer 컴포넌트 스타일링 수정
    
    * [Feat] PhocaContainerEx 수정 - sorting bar 추가
    
    * [Modify] 프로필 페이지 css 수정
    
    * [Feat] ProfileSetting 페이지 구현
    
    * [Feat] LoginInfo 마크업, 스타일링
    
    * [Modify] ProfileSetting 마크업 수정
    
    * [Feat] MyBias 경로 설정
    
    * [Feat] 최애 선택 페이지 마크업, 스타일링
    
    * [Feat] userEmail, userName 데이터 랜더링
    
    * [Feat] PhocaLikeButton 토글 기능 구현 및 이벤트 버블링 에러 fix
    
    * [Modify] 로그아웃 버튼 스타일 변경
    
    * [Modify] 주석 수정
    
    * [Modify] bias 스타일링 수정
    
    * [Modify] public 경로 수정
    
    * [Modify] PhocaLikeButton userData 추가
    
    * [Feat] PhpcaLikeButton 클릭시 유저의 likeList에 업데이트 기능 구현
    
    * [Feat] PhocaLikeButton 기능 구현 완료 - 찜 개수 업데이트, 찜 카드 목록 업데이트
    
    * [Feat]LikeDetail 컴포넌트 마크업
    
    * [Feat] LikeDetail 컴포넌트 기능 구현
    
    * [Modify] LikeDetail 컴포넌트 css 수정
    
    * [Modify] 주석 삭제
    
    * [Modify] bias 컴포넌트 스타일 수정
    
    * [Modify] BiasContainer 스타일 수정
    
    * [Modify] 포카 컨테이너 마진 수정
    
    * [Modify]Footer 마진 변경
    
    * [Modify] LogInfo 스타일 변경
    
    * [Feat] 로그아웃, 서비스 탈퇴 기능 구현
    
    * [Modify] MyBias 스타일 수정
    
    * [Modify] 최애 선택 페이지 스타일 변경
    
    * [Feat] 플로팅 버튼에 페이지 연결
    
    * [Modify] MyBias 기능 수정
    
    * [Feat].env 추가

--- src/pages/ProfileSetting/LoginInfo.jsx
+++ src/pages/ProfileSetting/LoginInfo.jsx
@@ -1,41 +1,107 @@
 import DetailHeader from '@/components/DetailHeader/DetailHeader';
-import userData from '../../loader/userData';
-import { useLoaderData } from 'react-router';
+import { useEffect, useState } from 'react';
+import PocketBase from 'pocketbase';
+import ConfirmationModal from '../../components/ConfirmationModal/ConfirmationModal';
+
+const pb = new PocketBase('https://shoong.pockethost.io');
 
 export default function LoginInfo() {
-  const user = useLoaderData();
-  console.log(user);
+  const [user, setUser] = useState({ email: '', username: '', id: '' });
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  useEffect(() => {
+    const authDataString = localStorage.getItem('auth');
+    if (!authDataString) return;
+
+    try {
+      const auth = JSON.parse(authDataString);
+      if (!auth?.user) return;
+
+      setUser({
+        email: auth.user.email,
+        username: auth.user.username,
+        id: auth.user.id,
+      });
+    } catch (error) {
+      console.error('Parsing authData error:', error);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    setIsLogoutModalOpen(false);
+    localStorage.setItem(
+      '로그인',
+      JSON.stringify({ init: false, collectBook: [] })
+    );
+    localStorage.removeItem('auth');
+    pb.authStore.clear();
+    window.location.href = '/login';
+  };
+
+  const handleDeleteAccount = async () => {
+    setIsDeleteModalOpen(false);
+    if (!user.id) {
+      alert('유저 ID를 찾을 수 없습니다.');
+      return;
+    }
+
+    try {
+      await pb.collection('users').delete(user.id);
+      handleLogout(); // 로그아웃 처리
+    } catch (error) {
+      console.error('Error deleting user account:', error);
+    }
+  };
+
   return (
     <div className="pt-12">
       <DetailHeader title="로그인 정보" />
-      <div className="flex min-h-screen flex-col">
+      <div className="flex  flex-col">
         <div className="flex flex-col items-start space-y-4 p-6">
           <div className="text-gray-700">
-            <p className="pb-2 pt-16 text-m03 font-m03 text-gray500">
+            <p className="pb-2 pt-16 text-m04 font-m04 text-gray500">
               이메일 주소
             </p>
-            <p className="text-m04 font-m04 text-gray-400">{user.email}</p>
+            <p className="text-b04 font-m04 text-gray300">{user.email}</p>
           </div>
           <div className="text-gray-700">
-            <p className="pb-2 pt-2 text-m03 font-m03 text-gray500">
+            <p className="pb-2 pt-2 text-m04 font-m04 text-gray500">
               고유 닉네임
             </p>
-            <p className="pb-2 text-m04 font-m04 text-gray-400">
+            <p className="pb-2 text-b04 font-m04 text-gray300">
               {user.username}
             </p>
           </div>
-          <div className="my-4 w-full border-t border-gray300"></div>
-          <p className="pt-2 text-m03 font-m03 text-gray500">소셜 로그인</p>
+          <div className="my-4 w-full border-t border-gray500"></div>
+          <p className="pt-2 text-m04 font-m04 text-gray500">소셜 로그인</p>
         </div>
-        <div className="mt-24 flex flex-col items-center justify-center gap-5 p-6">
-          <button className="rounded-lg border border-red-500 bg-white px-20 py-2 text-red-500 transition-all duration-300 hover:font-bold">
+        <div className="flex flex-col items-center justify-center gap-5 p-6">
+          <button
+            className="mt-32 rounded-lg border border-red-500 bg-white px-20 py-2 text-red-500 transition-all duration-300 hover:font-bold"
+            onClick={() => setIsLogoutModalOpen(true)}
+          >
             로그아웃
           </button>
-
-          <a href="#" className="text-gray300 underline">
+          <button
+            className="text-m04 font-m04 text-gray300 underline"
+            onClick={() => setIsDeleteModalOpen(true)}
+          >
             서비스 탈퇴하기
-          </a>
+          </button>
         </div>
+        <ConfirmationModal
+          isOpen={isLogoutModalOpen}
+          onClose={() => setIsLogoutModalOpen(false)}
+          onConfirm={handleLogout}
+          message="정말로 로그아웃하시겠습니까?"
+        />
+        <ConfirmationModal
+          isOpen={isDeleteModalOpen}
+          onClose={() => setIsDeleteModalOpen(false)}
+          onConfirm={handleDeleteAccount}
+          message="정말로 계정을 삭제하시겠습니까? 이 작업은 되돌릴 수 없습니다."
+        />
       </div>
     </div>
   );
