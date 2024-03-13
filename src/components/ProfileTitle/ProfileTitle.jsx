79f32384ad0ab26c3740ed7707fb44d3ad1d74da
pistapixie <hleehslee@gmail.com>
2024-03-14 00:19:38 +0900

    [Modify] Profile 페이지 ui 수정

--- src/components/ProfileTitle/ProfileTitle.jsx
+++ src/components/ProfileTitle/ProfileTitle.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function ProfileTitle() {
   return (
     <div className=" mr-2 flex flex-row items-center justify-between">
-      <h1 className="text-2xl font-bold text-contentPrimary">프로필</h1>
+      <h1 className="text-2xl font-bold text-gray-900">프로필</h1>
       <Link to="/profileSetting">
         <IoSettingsOutline className="h-10 text-3xl " />
       </Link>
