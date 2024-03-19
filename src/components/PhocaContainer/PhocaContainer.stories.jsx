d193bc1c2553265341b8e244e594d601cf7111ee
Choongman Kim <cm730712@gmail.com>
2024-03-20 00:52:36 +0900

    [Delete] storybook 패키지 삭제 (#107)
    * [Modify] 로고 변경
    
    * [Modify] 로고 이미지 수정
    
    * [Modify] 접근성 체크(1) 및 중복되는 css 변수로 적용
    
    * [Delete] storybook 삭제
    
    * [Delete] storybook 패키지 삭제
    
    * [Modify] checkJs: false로 변경

--- src/components/PhocaContainer/PhocaContainer.stories.jsx
+++
@@ -1,9 +0,0 @@
-import PhocaContainer from './PhocaContainer';
-
-/**@type{import('@storybook/react').Meta} */
-export default {
-  component: PhocaContainer,
-};
-
-/**@type{import('@storybook/react').StoryObj} */
-export const Default = {};
