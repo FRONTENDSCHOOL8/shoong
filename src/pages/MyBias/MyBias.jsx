e9b49e4eddb5f4bde7621156307b0de8917f2537
pistapixie <hleehslee@gmail.com>
2024-03-22 17:45:22 +0900

    [Modify] 푸터 내용 변경

--- src/pages/MyBias/MyBias.jsx
+++ src/pages/MyBias/MyBias.jsx
@@ -1,18 +1,45 @@
 import DetailHeader from '@/components/DetailHeader/DetailHeader';
 import SearchBar from '@/components/SearchBar/SearchBar';
 import { useLoaderData } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import PocketBase from 'pocketbase';
 
-/**
- *
- * @param {{items:string}} props
- * @returns
- */
+const pb = new PocketBase('https://shoong.pockethost.io');
 
 export default function MyBias({ items }) {
   const group = useLoaderData();
+  const [userId, setUserId] = useState(null);
 
-  const handleClick = (groupName) => {
+  useEffect(() => {
+    const authDataString = localStorage.getItem('auth');
+    if (!authDataString) return; // 조건 불충족 시 조기 리턴
+    try {
+      const authData = JSON.parse(authDataString);
+      if (!authData.user || !authData.user.id) return; // 조건 불충족 시 조기 리턴
+
+      setUserId(authData.user.id); // 조건 충족 시 상태 업데이트
+    } catch (error) {
+      console.error('Parsing authData error:', error);
+    }
+  }, []);
+
+  const handleClick = async (groupName) => {
     localStorage.setItem('bias', JSON.stringify({ init: groupName }));
+    if (!userId) {
+      console.error('User ID is missing');
+      return;
+    }
+
+    // 사용자의 biasGroup 필드 업데이트
+    try {
+      await pb.collection('users').update(userId, {
+        biasGroup: groupName,
+      });
+
+      console.log('biasGroup updated successfully');
+    } catch (error) {
+      console.error('Failed to update biasGroup', error);
+    }
   };
 
   return (
