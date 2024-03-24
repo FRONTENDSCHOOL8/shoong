535d0b5ebc016dd735ca744023b7ef2826286e6a
KIMCHOONGMAN <cm730712@gmail.com>
2024-03-24 19:20:00 +0900

    [Modify] JSDoc 삭제

--- src/components/ExchangeList/ExchangeList.jsx
+++ src/components/ExchangeList/ExchangeList.jsx
@@ -7,20 +7,6 @@ import ExchangeArticle from '../ExchangeListDetail/ExchangeArticle';
 import DetailHeader from '../DetailHeader/DetailHeader';
 import NumberOfExchangeList from '../ExchangeListDetail/NumberOfExchangeList';
 
-/**
- * ExchangeList 컴포넌트는 포토카드와 관련된 교환 글 목록을 표시하며, 각 교환 글의 작성자 정보를 함께 표시합니다.
- * 이 컴포넌트는 포토카드 데이터를 로드하고, 해당 데이터의 확장된 exchangeList 배열을 사용하여 교환 글을 렌더링합니다.
- *
- * @param {Object} props - 컴포넌트의 props 객체입니다.
- * @param {Object} props.photoCardData - 포토카드 데이터 객체입니다. 다음과 같은 필드를 포함해야 합니다:
- * @param {string} props.photoCardData.id - 포토카드의 고유 식별자입니다.
- * @param {object} props.photoCardData.expand
- * @param {Object[]} [props.photoCardData.expand.exchangeList] - 포토카드에 연관된 교환 글 객체의 배열입니다.
- * @param {React.ReactNode} [props.children] - 컴포넌트의 자식 요소들입니다.
- *
- * @returns {React.ReactNode} - ExchangeList 컴포넌트를 렌더링합니다.
- */
-
 export default function ExchangeList({ photoCardData }) {
   const [users, setUsers] = useState([]);
   const [exchangeListData, setExchangeListData] = useState(
@@ -48,7 +34,7 @@ export default function ExchangeList({ photoCardData }) {
 
           setUsers(usersData);
         } catch (error) {
-          // console.error('Error fetching users data:', error);
+          console.error('Error fetching users data:', error);
         }
       };
       fetchUsersData();
--- src/components/ExchangeListDetail/PhotoCardInfo.jsx
+++ src/components/ExchangeListDetail/PhotoCardInfo.jsx
@@ -3,26 +3,6 @@ import PhocaTitle from '../PhocaItem/PhocaTitle';
 import ArtistInfo from '../PhocaItem/ArtistInfo';
 import ArtistLogo from '../PhocaItem/ArtistLogo';
 
-/**
- * 포토카드의 세부 정보를 표시하는 컴포넌트입니다.
- * 포토카드 이미지, 로고 이미지, 그룹명, 멤버 이름, 제목 및 라벨 정보를 포함하여 표시합니다.
- * 각 세부 정보는 컴포넌트의 props를 통해 전달된 포토카드 데이터 객체에서 가져옵니다.
- *
- * @param {Object} props 컴포넌트의 props
- * @param {Object} props.photoCardData 포토카드에 대한 세부 정보를 포함하는 객체입니다.
- * @param {string} props.photoCardData.cardImg 포토카드 이미지의 파일명입니다.
- * @param {string} props.photoCardData.groupName 포토카드와 연관된 그룹명입니다.
- * @param {string} props.photoCardData.id 포토카드의 고유 식별자입니다.
- * @param {string} props.photoCardData.label 포토카드에 대한 라벨 정보입니다.
- * @param {string} props.photoCardData.logoImage 포토카드와 연관된 로고 이미지의 파일명입니다.
- * @param {string} props.photoCardData.memberName 포토카드에 등장하는 구성원의 이름입니다.
- * @param {string} props.photoCardData.title 포토카드의 제목입니다.
- * @param {object} props.photoCardData.expand
- * @param {Object[]} [props.photoCardData.expand.exchangeList]
- *
- * @returns {React.ReactNode} 포토카드 세부 정보를 렌더링하는 React 컴포넌트입니다.
- */
-
 export default function PhotoCardInfo({ photoCardData }) {
   const { cardImg, groupName, id, label, logoImage, memberName, title } =
     photoCardData;
